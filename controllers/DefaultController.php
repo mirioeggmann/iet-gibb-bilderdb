77b6f02b4cfd1f4e120001367c3514c5af43d8cc
Mirio Eggmann <jmelab@protonmail.ch>
2016-04-21 23:12:28 +0200

    Added some additional stuff and edited the database model.

---
+++ controllers/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+class DefaultController extends Controller
+{
+	public function __construct()
+	{
+		$view = new View('header', array('title' => 'Startseite', 'heading' => 'Startseite'));
+		$view->display();
+	}
+
+	public function index()
+	{
+		$view = new View('default_index');
+		$view->display();
+	}
+
+	public function __destruct()
+	{
+		$view = new View('footer');
+		$view->display();
+	}
+}
--- docs/jmepic.sql
+++ docs/jmepic.sql
@@ -8,7 +8,7 @@
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
-  id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id        INT NOT NULL AUTO_INCREMENT,
   firstName VARCHAR(64)  NULL,
   lastName  VARCHAR(64)  NULL,
   userName  VARCHAR(64) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE user (
 
 DROP TABLE IF EXISTS gallery;
 CREATE TABLE gallery (
-  id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id		INT NOT NULL AUTO_INCREMENT,
   name		VARCHAR(64) NOT NULL,
   PRIMARY KEY (id)
 );
@@ -27,8 +27,8 @@ CREATE TABLE gallery (
 -- maybe thumbnail not?
 DROP TABLE IF EXISTS picture;
 CREATE TABLE picture (
-  id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  original			VARCHAR(64) NOT NULL,
+  id			INT NOT NULL AUTO_INCREMENT,
+  original		VARCHAR(64) NOT NULL,
   thumbnail 		VARCHAR(70) NOT NULL,
   height_original	INT NOT NULL,
   width_original	INT NOT NULL,
@@ -36,29 +36,35 @@ CREATE TABLE picture (
   height_thumbnail	INT NOT NULL,
   width_thumbnail	INT NOT NULL,
   size_thumbnail	INT NOT NULL,
-  date  	DATETIME,
-  PRIMARY KEY (id)
+  date  		DATETIME,
+  gallery_id		INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (gallery_id) REFERENCES gallery(id)
 );
 
 DROP TABLE IF EXISTS tag;
 CREATE TABLE tag (
-  id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id		INT NOT NULL AUTO_INCREMENT,
   tag		VARCHAR(64) NOT NULL,
   PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS user_gallery;
 CREATE TABLE user_gallery (
-  id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  userId	INT NOT NULL,
-  galleryId INT NOT NULL,
-  PRIMARY KEY (id)
+  id		INT NOT NULL AUTO_INCREMENT,
+  user_id	INT NOT NULL,
+  gallery_id 	INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (gallery_id) REFERENCES gallery(id)
 );
 
 DROP TABLE IF EXISTS picture_tag;
 CREATE TABLE picture_tag (
-  id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  pictureId INT NOT NULL,
-  tagID		INT NOT NULL,
-  PRIMARY KEY (id)
+  id		INT NOT NULL AUTO_INCREMENT,
+  picture_id 	INT NOT NULL,
+  tag_id	INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (picture_id) REFERENCES picture(id),
+  FOREIGN KEY (tag_id) REFERENCES tag(id)
 );
